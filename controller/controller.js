87de89a1792b3a96b74285520b1dc1aa86b193e5
gyeomnii <a01020799303@gmail.com>
2024-05-12 14:40:22 +0900

    v1

--- controller/controller.js
+++ controller/controller.js
@@ -2,14 +2,14 @@ import User from "../schemas/user.js"
 
 const main = (req, res)=>{
     res.set({'Content-Type' : 'text/html; charset=utf-8'});
-    res.send('<h1>Welcome Express !! 자동배포완성!</h1>')
+    res.send('<h1>Welcome Express !!! 자동배포완성!</h1>')
 }
 
-const test = async() =>{
+const test = async(req,res) =>{
   // 몽고DB에 있는 유저정보를 보내준다.
   const users = await User.find()
   console.log(users)
   res.json(users)
-}
+};
 
-export {main, test}
+export {main, test};
